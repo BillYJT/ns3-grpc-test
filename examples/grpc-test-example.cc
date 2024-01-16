134c361a07aa90f7933550a5ddf242b08bcf0dfc
BillTao-T14 <toyjet8@gmail.com>
2024-01-16 17:42:33 +0900

    working cmake config for GRPC integration

--- examples/grpc-test-example.cc
+++ examples/grpc-test-example.cc
@@ -11,7 +11,8 @@
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
-#include "../model/proto_generated_file/helloworld.grpc.pb.h"
+// #include "../model/proto_generated_file/helloworld.grpc.pb.h"
+#include "ns3/grpc-test-module.h"
 
 #include "ns3/core-module.h"
 #include "ns3/grpc-test-helper.h"
--- model/grpc-test.h
+++ model/grpc-test.h
@@ -1,18 +1,26 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 #ifndef GRPC_TEST_H
 #define GRPC_TEST_H
-
-// #include "absl/flags/flag.h"
-// #include "absl/flags/parse.h"
-// #include "absl/strings/str_format.h"
-
-// #include <grpcpp/ext/proto_server_reflection_plugin.h>
-// #include <grpcpp/grpcpp.h>
-// #include <grpcpp/health_check_service_interface.h>
-// #include "../model/proto_generated_file/helloworld.grpc.pb.h"
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_format.h"
+// 
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+// #include <helloworld.grpc.pb.h>
+#include "helloworld.grpc.pb.h"
+// #include <proto_generated_file/helloworld.grpc.pb.h>
 
 namespace ns3 {
 
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
 /* ... */
 
 }
